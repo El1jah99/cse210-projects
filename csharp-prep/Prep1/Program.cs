5156f442760d4333e4a25429e0606fccfe32e0b0
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-01-07 16:24:07 -0700

    Completed assignment 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,8 +6,10 @@ class Program
     {
         Console.WriteLine("What is your first name?");
         string first_name = Console.ReadLine();
+
         Console.WriteLine("What is your last name?");
         string last_name = Console.ReadLine();
-        Console.WriteLine($"{last_name},{first_name} {last_name}");
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercent = Console.ReadLine();
+
+        int gradePercentInt = int.Parse(gradePercent);
+        
+        string letterGrade = "";
+
+        if (gradePercentInt >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentInt >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentInt >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentInt >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade}");
+
+        if (gradePercentInt >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed. Try again next semester.");
+        }
+
+
+            }
+
 }
