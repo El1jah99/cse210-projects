b5b19d27e4a38d119fbf68f3acbd535d7739c320
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-01-11 12:04:04 -0700

    Completed assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,100);
+
+
+
+        int guess = -1;
+        int guessCount = 1;
+
+        while (guess != randomNumber)
+        {   
+            Console.WriteLine("Take a guess at the number I'm thinking of: ");
+            guess = int.Parse(Console.ReadLine());
+            
+            
+            guessCount += 1;
+        
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+                
+                
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Nailed it!");
+                Console.WriteLine($"It took you {guessCount} guesses");
+            }
+
+        
+        }
     }
 }
