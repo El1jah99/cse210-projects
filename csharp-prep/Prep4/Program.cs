1e24c8ad31357d2872958db200329c38d042cd97
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-01-14 13:35:03 -0700

    Completed assignement

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       List<int> numbers = new List<int>();
+
+       int userInput = -999;
+        
+        Console.WriteLine("Enter positive integers - Type 0 when finished.");
+
+       while (userInput != 0)
+       {
+            
+            Console.WriteLine("Enter a positive integer");
+            
+            userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+
+       }
+
+
+        int total = numbers.Sum();
+        
+
+        Console.WriteLine($"The total is {total}");
+
+        float average = ((float)total) / numbers.Count;
+
+        Console.WriteLine($"The average is {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is {max}");    
+
     }
 }
