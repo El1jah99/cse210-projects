7a5ca0816036c11d132be8e35f9cbac157cabfaa
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-01-19 22:49:06 -0700

    completed assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2017;
+        job1._endYear = 2020;
+        
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Systems Engineer";
+        job2._company = "NVIDIA";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+        
+
+        Resume resume = new Resume();
+
+        resume._name = "Elijah M";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+
+        resume.DisplayJobs();
+
+
+
+
+
+        
+
+        
+
+
     }
 }
