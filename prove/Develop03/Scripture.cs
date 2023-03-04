684094ad595a66bc4e9d3b46d08058883a359bd7
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-03-04 13:32:32 -0700

    completed

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -34,7 +34,7 @@ public class Scripture
     public void AddToDictionary()
     {
         scriptureDictionary.Add(_reference, _text);
-        //scriptureDictionary.Add("John 3:16", "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        
     }
 
     public string DisplayWords(string reference)
