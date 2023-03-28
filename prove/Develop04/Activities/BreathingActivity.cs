a359db5f15340ff0b1ea19b5edd3fbe6323ec0c9
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-03-28 03:36:40 -0600

    Minor edits

--- prove/Develop04/Activities/BreathingActivity.cs
+++ prove/Develop04/Activities/BreathingActivity.cs
@@ -5,6 +5,7 @@ public class BreathingActivity : Activity
 
     public BreathingActivity()
     {
+        
         SetActivityName("Breathing Activity");
 
         SetActivityDescription("This activity will help you focus on your breathing. Try to clear your mind and focus on taking deep breaths.");
@@ -19,12 +20,13 @@ public class BreathingActivity : Activity
     private void StartBreathing()
     {
 
-        int repetitions = (GetActivityDuration() / 10) + 1;
+        int repetitions = (GetActivityDuration() / 12) + 1;
+
         for (int i = 0; i < repetitions; i++)
         {
 
             StartTimer().DisplayCountdown("Breathe in...", 6);
-            StartTimer().DisplayCountdown("Breathe out... ", 4);
+            StartTimer().DisplayCountdown("Breathe out... ", 6);
 
         }
 
--- prove/Develop04/Commands/Command.cs
+++ prove/Develop04/Commands/Command.cs
@@ -6,6 +6,7 @@ class Command
 {
     public virtual void Execute()
         {
+            
             throw new Exception("Sorry, that activity isn't available");
         }
 
