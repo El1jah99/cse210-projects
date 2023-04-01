84d442de2191c2958bad6ad1e505ae73e69c976e
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-04-01 16:50:14 -0600

    Added quizzes

---
+++ final/FinalProject/Commands/TakeQuizCommand.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+class TakeQuizCommand : Command
+{
+    
+    public override void Execute()
+    {
+        StudySession studysession = new TakeQuiz();
+
+        studysession.StartStudySession();
+        studysession.EndStudySession();
+
+
+    }
+
+
+
+
+
+}
