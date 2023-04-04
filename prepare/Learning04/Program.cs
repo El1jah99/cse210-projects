e0c6cc886aee9773a68dccb4496cf697e6cb380e
Elijah M <100311820+El1jah99@users.noreply.github.com>
2023-04-03 18:59:05 -0600

    completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment a1 = new Assignment("Elijah Miller", "Computer Science");
+        Console.WriteLine(a1.GetSummary());
+
+        
+        MathAssignment a2 = new MathAssignment("Elijah Miller", "Fractions", "8.2", "5-28");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Elijah Miller", "American History", "The Great Depression");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
+
+
     }
 }
